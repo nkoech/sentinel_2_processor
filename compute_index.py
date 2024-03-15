071f78e71a1f1224197c75bb52dde7bb326b98b7
nkoech <koech.nicholas@gro-intelligence.com>
2024-03-15 14:28:33 +0300

    Commented formulas

--- compute_index.py
+++ compute_index.py
@@ -13,14 +13,17 @@ def compute_index(band_1: np.ndarray, band_2: np.ndarray, no_data: float) -> np.
 
 
 def compute_ndvi(bands: Bands, no_data: float) -> np.ndarray:
+    # Formula: NDVI = (NIR - Red) / (NIR + Red)
     return compute_index(bands.nir, bands.red, no_data)
 
 
 def compute_ndwi(bands: Bands, no_data: float) -> np.ndarray:
+    # Formula: NDWI = (Green - NIR) / (Green + NIR)
     return compute_index(bands.green, bands.nir, no_data)
 
 
 def compute_evi(bands: Bands, no_data: float) -> np.ndarray:
+    # Formula: EVI = 2.5 * (NIR - Red) / (NIR + 6 * Red - 7.5 * Blue + 1)
     # Convert band to float so that floating point math is used.
     red_band = bands.red.astype(np.float64)
     denominator = bands.nir + 6 * red_band - 7.5 * bands.blue + 1
