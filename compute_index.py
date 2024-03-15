5e06df124b603ad41ab0382df9c7782b31d382c0
nkoech <koech.nicholas@gro-intelligence.com>
2024-03-15 14:26:59 +0300

    Added evi compute

--- compute_index.py
+++ compute_index.py
@@ -18,3 +18,13 @@ def compute_ndvi(bands: Bands, no_data: float) -> np.ndarray:
 
 def compute_ndwi(bands: Bands, no_data: float) -> np.ndarray:
     return compute_index(bands.green, bands.nir, no_data)
+
+
+def compute_evi(bands: Bands, no_data: float) -> np.ndarray:
+    # Convert band to float so that floating point math is used.
+    red_band = bands.red.astype(np.float64)
+    denominator = bands.nir + 6 * red_band - 7.5 * bands.blue + 1
+    # Mask to avoid ZeroDivisionError
+    denominator = np.ma.masked_where(denominator == 0, denominator)
+    evi_data = 2.5 * ((bands.nir - red_band) / denominator)
+    return evi_data.filled(no_data)
