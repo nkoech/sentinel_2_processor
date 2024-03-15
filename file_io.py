0a6a2638c122a87a852297a28c61a2dd573c0f8a
nkoech <koech.nicholas@gro-intelligence.com>
2024-03-15 13:52:44 +0300

    Added function to read geojson file

--- file_io.py
+++ file_io.py
@@ -8,3 +8,8 @@ def read_dataset(input_file: pathlib.Path) -> gdal.Dataset:
     if dataset is None:
         raise RuntimeError(f"Could not open the file: {input_file}")
     return dataset
+
+
+def read_geojson(input_file: pathlib.Path, target_crs: int) -> gdal.Dataset:
+    geom = gpd.read_file(input_file)
+    return geom.to_crs(epsg=target_crs)
