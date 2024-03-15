43bcc9bc13ced4a19a938aa71f1ef74a256242ab
nkoech <koech.nicholas@gro-intelligence.com>
2024-03-15 14:23:50 +0300

    Added feature to compute NDVI

--- compute_index.py
+++ compute_index.py
@@ -1,6 +1,8 @@
 import numpy as np
 
 
+from sentinel_2_processor import Bands
+
 def compute_index(band_1: np.ndarray, band_2: np.ndarray, no_data: float) -> np.ndarray:
     # Convert band to float so that floating point math is used.
     band_1 = band_1.astype(np.float64)
@@ -8,3 +10,7 @@ def compute_index(band_1: np.ndarray, band_2: np.ndarray, no_data: float) -> np.
     band_1 = np.ma.masked_where(band_1 + band_2 == 0, band_1)
     index = (band_1 - band_2) / (band_1 + band_2)
     return index.filled(no_data)
+
+
+def compute_ndvi(bands: Bands, no_data: float) -> np.ndarray:
+    return compute_index(bands.nir, bands.red, no_data)
